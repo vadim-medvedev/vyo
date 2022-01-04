6b58754d6be2a9f751739213794f835219909f3a
Vadim Medvedev <vadim.medvedev.work@gmail.com>
2022-01-04 21:20:33 +0300

    add some gradle dependencies;

---
+++ src/main/java/com/yotewo/vyo/VyoApplication.java
@@ -0,0 +1,13 @@
+package com.yotewo.vyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VyoApplication.class, args);
+    }
+
+}
